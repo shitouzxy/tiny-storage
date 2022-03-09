483f07d1f07beead801acdcdd1005723bcb63bd5
Sean <shichuanlong@100doc.com.cn>
2022-03-09 23:39:42 +0800

    change  vite publish build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+const path = require('path')
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/libs/storage.ts'),
+      name: 'tinyStore',
+    }
+  }
 })
