2395a849ccbe6df7a6222641d091dab3948302bf
Sean <shichuanlong@100doc.com.cn>
2022-03-12 23:39:00 +0800

    add: add cookie handler support

--- src/libs/Storage.ts
+++ src/libs/Storage.ts
@@ -1,24 +1,47 @@
 
+// const jscookie = require('jscookie');
+import * as jscookie from 'jscookie'
+
 type storageData = {
     data: any,
-    expires?: number
+    expires?: string
+}
+
+type eventsCenter = {
+    [key:string]: Array<Function> | []
 }
 
 export default class Storage {
     protected _prefix: string = '';
+    protected eventsCenter: eventsCenter = {};
     constructor(prefix?: string) {
-        this._prefix = prefix || '';
+        this._prefix = prefix || 'tinyStorage_';
     }
     /**
      * @description 设置缓存
      */
-    setItem(key:string, data: any, expired?: number) {
-        // To Do 增加过期时间设置 - 存储方法跟cookie 原生设置方法一致
-        if(localStorage) {
+    setItem(key:string, data: any, exdays?: number) {
+        if (localStorage) {
             const storageData:storageData = { data };
+            if(exdays) {
+                var d = new Date();
+				d.setTime( d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+                storageData.expires = d.toUTCString();
+            }
             localStorage.setItem(this._prefix + key, JSON.stringify(storageData));
             // this.emit(key, data);
+        } else {
+            const storageData:storageData = { data };
+            jscookie.set({
+                name: key,
+                value: JSON.stringify(storageData),
+                exdays,
+                path: '/' // 全局路径
+            })
         }
+
+        // 派发事件 - 当前存储值已更新
+        this._emit(key, data);
     }
     /**
      *
@@ -26,11 +49,28 @@ export default class Storage {
      * @param data {any}
      * @returns {void}
      */
-    emit(key:string, data: any) {
+    private _emit(key:string, data: any) {
+        const events:Array<Function> = this.eventsCenter[key];
+        if(events) {
+            events.forEach((callback: Function) => {
+                callback(data);
+            })
+        } else {
+            console.log('unexcepted events listener');
+        }
     }
 
     on(key:string, callback: Function) {
-
+        //
+        if(this.eventsCenter[key]) {
+            const events:Array<Function> = this.eventsCenter[key];
+            events.push(callback);
+            this.eventsCenter[key] = events;
+        } else {
+            const events:Array<Function> = [];
+            events.push(callback);
+            this.eventsCenter[key] = events;
+        }
     }
     /**
      * @description 获取缓存
@@ -40,12 +80,21 @@ export default class Storage {
             let data:string|null = localStorage.getItem(this._prefix + key);
             if(data) {
                 const result: storageData = JSON.parse(data);
+                // check expires
+                if(result.expires) {
+                    const expires = new Date(result.expires);
+                    // 如果已经过期了， 那直接删除
+                    if(expires < new Date()) {
+                        this.removeItem(key);
+                        return null;
+                    } else {
+                        return result.data;
+                    }
+                }
                 return result.data;
             } else {
                 return null;
             }
-            // to do Check 过期
-
         }
         return null;
     }
@@ -55,6 +104,8 @@ export default class Storage {
     removeItem(key:string) {
         if(localStorage) {
             localStorage.removeItem(this._prefix + key);
+        } else {
+            jscookie.remove(key);
         }
     }
 
@@ -62,6 +113,6 @@ export default class Storage {
      * @description 清空缓存
      */
     clearAll() {
-        localStorage.clear()
+        localStorage.clear();
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Storage from './libs/Storage'
+
+
+const store = new Storage()
+store.setItem('tiny', {hello: 'world'});
+
+const test = store.getItem('tiny');
+
+console.log(test);
+
 
 createApp(App).mount('#app')
